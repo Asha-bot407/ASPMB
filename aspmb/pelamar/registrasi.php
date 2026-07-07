276ae50d4e8d2a9f0b5ca1f86c7654be5917bcde
Asha-bot407 <ludwiggrimm85@gmail.com>
2026-07-07 11:41:08 +0800

    part3

---
+++ aspmb/pelamar/registrasi.php
@@ -0,0 +1,184 @@
+<?php
+
+    session_start();
+
+    require "../fungsi.php";
+
+    if(!isset($_SESSION['login'])){
+        header("Location: ../login.php");
+        exit;
+    }
+
+    if($_SESSION['peran'] != "pelamar"){
+        header("Location: ../login.php");
+        exit;
+    }
+
+    if(isset($_POST['registrasi'])){
+
+    $hasil = registrasi($_POST);
+
+    if($hasil > 0){
+        echo "<script>
+        alert('Data berhasil disimpan');
+        document.location.href='registrasi.php';
+        </script>";
+    }else{
+        echo "<script>alert('Data gagal disimpan');</script>";
+    }
+    }
+?>
+
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Pelamar</title>
+
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
+    <link rel="stylesheet"
+    href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.13.1/font/bootstrap-icons.min.css">
+    <link rel="stylesheet" href="css/style.css">
+</head>
+<body>
+    
+<nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
+    <div class="container">
+
+        <a class="navbar-brand fw-bold" href="index.php">
+            ASPMB
+        </a>
+
+        <button class="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarNav">
+
+            <span class="navbar-toggler-icon"></span>
+
+        </button>
+
+        <div class="collapse navbar-collapse"
+             id="navbarNav">
+
+            <ul class="navbar-nav ms-auto">
+
+
+                <li class="nav-item">
+                    <a class="nav-link" href="index.php">
+                        Beranda
+                    </a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link" href="../logout.php">
+                        Logout
+                    </a>
+                </li>
+
+                
+
+            </ul>
+
+        </div>
+
+    </div>
+</nav>
+
+<div>
+    <h2>DASHBOARD REGISTRASI</h2>
+</div>
+
+<div>
+    <?php
+        echo "Selamat Datang <strong>" . $_SESSION['username'] . "</strong>";
+    ?>
+</div>
+
+<div>
+    <ul>
+        <li>
+            <a href="registrasi.php">Registrasi</a>
+        </li>
+         <li>
+            <a href="unggahDok.php">Dokumen</a>
+        </li>
+         <li>
+            <a href="pengumuman.php">Pengumuman</a>
+        </li>
+        <li>
+            <a href="kelulusan.php">Kelulusan</a>
+        </li>
+    </ul>
+</div>
+
+
+<div>
+        <hr>
+        <p>Silahkan isi biodata anda</p>
+        <?php 
+            $username = $_SESSION['username'];
+            
+            if(cekRegistrasi($username) == 0):
+           
+        ?>
+        <form action="" method="POST">
+            <label for="namaDepan">Nama Depan</label>
+            <input type="text" name="namaDepan" id="namaDepan" required>
+
+            <label for="namaBelakang">Nama Belakang</label>
+            <input type="text" name="namaBelakang" id="namaBelakang" required> <br><br>
+
+            <label for="tempatLahir">Tempat Lahir</label>
+            <input type="text" name="tempatLahir" id="tempatLahir" required> <br><br>
+
+            <label for="tglLahir">Tanggal Lahir</label>
+            <input type="date" name="tglLahir" id="tglLahir" required> <br><br>
+
+            <label for="jenisKelamin">Jenis Kelamin</label>
+            <input type="radio" name="jenisKelamin" id="Laki-laki" value="Laki-laki" required>
+            <label for="Laki-laki">Laki-laki</label>
+            <input type="radio" name="jenisKelamin" id="Perempuan" value="Perempuan">
+            <label for="Perempuan">Perempuan</label> <br><br>
+
+            <label for="nisn">NISN</label>
+            <input type="text" name="nisn" id="nisn" maxlength="10" required> <br><br>
+
+            <label for="agama">Agama</label>
+            <select name="agama" id="agama">
+                <option value="Islam">Islam</option>
+                <option value="Hindu">Hindu</option>
+                <option value="Protestan">Protestan</option>
+                <option value="Katholik">Katholik</option>
+                <option value="Budha">Budha</option>
+            </select> <br><br>
+
+            <label for="asalSekolah">Asal Sekolah</label>
+            <input type="text" name="sekolahAsal" id="asalSekolah" required> <br><br>
+
+            <label for="alamat">Alamat</label>
+            <textarea name="alamat" id="alamat" required></textarea> <br><br>
+
+            <label for="telepon">Telepon/WA</label>
+            <input type="text" name="telepon" id="telepon" required>
+
+            <button type="submit" class="btn btn-primary" name="registrasi">Submit</button>
+            
+
+        </form>
+        
+        <?php 
+            else:
+                echo "Anda sudah mengisi formulir";
+            
+            endif;
+        ?>
+            
+    
+</div>
+
+   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous"></script>
+</body>
+</html>
